d5bb046b0a91d0b4131264fa3301ac6d674605fd
Scotty Logan <scotty@catbert.net>
2014-11-23 17:46:30 -0800

    adding dependencies to browser class

--- manifests/browser.pp
+++ manifests/browser.pp
@@ -13,30 +13,34 @@ class java::browser (
   if $ensure == 'present' {
 
     file { '/Library/Application Support/Oracle/Java/javaws':
-      ensure => link,
-      owner  => 'root',
-      group  => 'wheel',
-      target => '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/_javaws',
+      ensure  => link,
+      owner   => 'root',
+      group   => 'wheel',
+      target  => '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/_javaws',
+      require => Package['java-jre'],
     }
 
     file { '/Library/Application Support/Oracle/Java/Info.plist':
-      ensure => link,
-      owner  => 'root',
-      group  => 'wheel',
-      target => '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Enabled.plist',
+      ensure  => link,
+      owner   => 'root',
+      group   => 'wheel',
+      target  => '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Enabled.plist',
+      require => Package['java-jre'],
     }
 
   } else {
 
     file { '/Library/Application Support/Oracle/Java/javaws':
-      ensure => absent,
+      ensure  => absent,
+      require => Package['java-jre'],
     }
 
     file { '/Library/Application Support/Oracle/Java/Info.plist':
-      ensure => link,
-      owner  => 'root',
-      group  => 'wheel',
-      target => '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Disabled.plist',
+      ensure  => link,
+      owner   => 'root',
+      group   => 'wheel',
+      target  => '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Disabled.plist',
+      require => Package['java-jre'],
     }
 
   }
