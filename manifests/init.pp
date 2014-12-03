7cc8194c285b270d960ccd1e3411f95eadcb9d30
Scotty Logan <swl@stanford.edu>
2014-12-03 03:16:26 -0800

    appeasing the finicky puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,7 @@ class java (
   $jre_url = "${base_download_url}/jre-${dmg_sfx}"
   $jdk_url = "${base_download_url}/jdk-${dmg_sfx}"
   $wrapper = "${boxen::config::bindir}/java"
-  $jvm_dir = "/Library/Java/JavaVirtualMachines"
+  $jvm_dir = '/Library/Java/JavaVirtualMachines'
   $jdk_dir = "${jvm_dir}/jdk1.7.0_${update_version}.jdk"
   $sec_dir = "${jdk_dir}/Contents/Home/jre/lib/security"
   $bdl_dir = "${jdk_dir}/Contents/Home/bundle"
